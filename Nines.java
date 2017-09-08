e32dd55f04d97cc83fe5a9b0218bd4ca61878bcf
高橋佳佑 <takahashi.keisuke@m.alhinc.jp>
2017-09-08 14:45:41 +0900

    first commit 課題　九九の一覧表示プログラム

---
+++ Nines.java
@@ -0,0 +1,15 @@
+
+public class Nines {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=9; i++){
+			for(int j=1; j<=9; j++){
+				int sum = i * j;
+				System.out.print(sum);
+				System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+}
